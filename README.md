e19453101cbfd052d1b8cd780a15875813870210
Walter Tlatenchi <walter.tlatenchi@gmail.com>
2017-11-26 15:46:25 -0800

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # nodecsvtojson
-EdX Module 1 Assignment Lab: CSV to JSON Converter
+EdX Module 1 Assignment Lab: CSV to JSON Converter. Will convert CSV to JSON file.
+
+# How to Run
+`node app.js`
---
+++ app.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const path = require('path');
+const csv = require('csvtojson');
+
+function convertCSVtoJSON(url = './customer-data.csv') {
+  fs.readFile(url, 'utf8', function(err, data) {
+    if(err) {
+      return console.error(err);
+    }
+    const fileName = url.replace('.csv', '.json');
+    csv()
+    .fromString(data)
+    .on('end_parsed', (jsonArrObj)=> {
+      fs.writeFileSync(path.join(__dirname, fileName), JSON.stringify(jsonArrObj));
+    });
+  });
+}
+
+convertCSVtoJSON(process.argv[2]);
